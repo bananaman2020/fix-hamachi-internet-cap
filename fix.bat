01250672d1a4f192ce1a16ce422e3aeef821670d
Kuba <kuba@Kkgmedia.com>
2022-04-06 15:08:50 +0100

    initial release

---
+++ fix.bat
@@ -0,0 +1,13 @@
+echo Listing current netsh settings
+
+netsh interface tcp show global
+
+echo Setting autotuning level to normal...
+
+netsh interface tcp set global autotuninglevel=normal
+
+echo Enabling RSS....
+
+netsh interface tcp set global rss=enabled
+
+echo Settings have been applied. Try testing your internet now. Script from github.com/bananaman2020/fix-hamachi-internet-cap
